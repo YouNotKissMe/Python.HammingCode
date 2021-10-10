d6f49781e9098af5820fae1f1551a38fb27b5820
Ougi-kun <71519415+YouNotKissMe@users.noreply.github.com>
2021-10-10 18:43:03 +0300

    код Хэмминга

---
+++ main.py
@@ -0,0 +1,83 @@
+a = input('Введите комбинацию 0 и 1: ')
+mda = [i for i in a]
+# print(mda)
+r = 0
+while 2 ** r < len(a) + r + 1:
+    r += 1
+# print(r)
+massive = []
+q = ''
+c = 0
+for i in range(r):
+
+    while len(q) < (len(a) + r):
+        q += ('0' * (2 ** c))
+        q += ('1' * (2 ** c))
+    massive.append(q)
+    q = ''
+    c += 1
+q = ''
+realvassive = []
+for i in range(len(a) + r + 1):
+    for z in range(r):
+        q += massive[z][i]
+    realvassive.append(q)
+    q = ''
+realvassive.pop(0)
+print(realvassive, '\n', len(realvassive))
+'''создали таблицу '''
+massivestepeney = []
+m=0
+while 2**m < len(realvassive)+1:
+    massivestepeney.append(2**m)
+    m += 1
+m=0
+massiveQ = [x for x in range(len(realvassive))]
+print(len(massiveQ))
+for i in massiveQ:
+    if i + 1 in massivestepeney:
+        massiveQ[i] = 'None'
+    else:
+        massiveQ[i] = int(mda[m])
+        m += 1
+index = []
+for i in range(len(massiveQ)):
+    if massiveQ[i] != 'None':
+        index.append(i)
+print(index)
+m1 = []
+m = 0
+for i in range(len(massivestepeney)):
+    for b in index:
+
+        m += massiveQ[b] * int(realvassive[b][i])
+    m1.append(m % 2)
+    m = 0
+for i in range(len(m1)):
+    massiveQ[massiveQ.index('None')] = m1[i]
+a=int(input(f'{massiveQ} напишите порядковый номер числа в котором нашкодить: '))
+if massiveQ[a-1] == 1:
+    massiveQ[a-1] = 0
+else:
+    massiveQ[a-1] = 1
+mz=0
+m23=[]
+for i in range(len(massivestepeney)):
+    for b in range(len(massiveQ)):
+        mz += massiveQ[b]*int(realvassive[b][i])
+    m23.append(mz % 2)
+    mz=0
+print(m23)
+qzq=0
+for i in range(len(m23)):
+    if m23[i] != 0:
+        qzq += 2**i
+print(f'ошибка в {qzq} , нашкодиный вариант {massiveQ}')
+if massiveQ[qzq-1] == 1:
+    massiveQ[qzq-1] = 0
+else:
+    massiveQ[qzq-1] = 1
+print('исправленный вариант: ', massiveQ)
+
+
+
